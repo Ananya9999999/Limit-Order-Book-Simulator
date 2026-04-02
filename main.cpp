bc3133099eb94d923c67f47a21e57100aadf9827
Ananya Singla <imananya07@gmail.com>
2026-04-02 22:10:18 +0530

    Adding the order

--- main.cpp
+++ main.cpp
@@ -9,3 +9,22 @@ struct Order{
     bool isBuy;
     std::chrono::steady_clock::time_point timestamp;
 };
+
+class LimitOrderBook{
+    private:
+        std::map<double, std::deque<Order>, std::greater<double>> bids;
+        std::map<double, std::deque<Order>> asks;
+
+        uint64_t nextId= 1;
+    public:
+        void addOrder(bool isBuy, double price, uint64_t volume){
+            Order order{nextId++, volume, price, isBuy, std::chrono::steady_clock::now()};
+
+            if (isBuy){
+                bids[price].emplace_back(order);
+            }
+            else{
+                asks[price].emplace_back(order);
+            }
+        }
+}
